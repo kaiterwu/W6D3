f1cda2dbf55053a0e7a3b48b451888ae1233da3e
Fahim Khan <fk652@nyu.edu>
2023-02-15 16:37:48 -0500

    finish first routes project

--- routes_controllers/app/controllers/users_controller.rb
+++ routes_controllers/app/controllers/users_controller.rb
@@ -1,14 +1,50 @@
 class UsersController < ApplicationController
     def index
         # render plain: "I'm in the index action!"
-        render json: params 
-    end 
-
-    def create
-        render json: params 
+        @users = User.all
+        render json: @users 
     end 
 
     def show 
-        render json: params 
+        begin
+            @user = User.find(params[:id])
+            render json: @user
+        rescue
+            render json: 'invalid id', status: :unprocessable_entity
+        end
     end 
+
+    # params need to be nested within a user hash
+    # key = user[name]  value = 'Alvin'
+    def create
+        @user = User.new(params.require(:user).permit(:name, :email))
+        if @user.save
+          render json: @user
+        else
+          render json: @user.errors.full_messages, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            redirect_to user_url(@user.id)
+        else
+            render json: @user.errors.full_messages, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @user = User.find(params[:id])
+        if @user.destroy
+            render json: "user destroyed"
+        else
+            render json: @user.errors.full_messages, status: :unprocessable_entity
+        end
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:email, :name)
+    end
 end
